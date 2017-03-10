b3a2aaa640fad3fa9382da587a73c0695bdf58aa
Ravali Ginjupalli <ravali.ginjupalli@gmail.com>
2017-03-10 17:58:07 -0600

    feat: Add List controller

---
+++ WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public ProductController(IProductsRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
